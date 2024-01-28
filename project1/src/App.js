7474c6e049f556e49ff9760765dc7b1e01f91b61
Manasa Y <144911557+ManasaYerra@users.noreply.github.com>
2024-01-28 11:31:54 +0530

    Boiler Plate

---
+++ project1/src/App.js
@@ -0,0 +1,23 @@
+import logo from "./kaLion.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <div id="info">
+        <h2>
+          {" "}
+          Pawan Kalyan <br />
+          <a href="https://en.wikipedia.org/wiki/Pawan_Kalyan">
+            Click here to know more info.
+          </a>
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
